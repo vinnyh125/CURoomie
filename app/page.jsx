3e20ce8763871754c88a401fa1a5d227213db9db
NotVinnny <vh222@cornell.edu>
2024-04-17 23:04:52 -0400

    File rename

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <>
+      <div className="flex min-w-screen min-h-screen items-center justify-center">
+        <div>
+          Hello there, roommate. You're stuck with me for the rest of the year.
+        </div>
+      </div>
+    </>
+  );
+}
