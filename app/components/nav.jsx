3a6f4fb72762e07fbbd6d8cf9b0242a60eb58e17
NotVinnny <vh222@cornell.edu>
2024-04-17 23:06:17 -0400

    Navbar and Layout
    + set up basically navbar code structure
    + imported navbar component into layout file so as to not re-render inefficiently

---
+++ app/components/nav.jsx
@@ -0,0 +1,14 @@
+export default function Nav() {
+  return (
+    <>
+      <div className="flex min-w-screen justify-between">
+        <div>
+          CURoomie
+        </div>
+        <div>
+          Other Links
+        </div>
+      </div>
+    </>
+  )
+}
---
+++ app/layout.jsx
@@ -0,0 +1,26 @@
+import { Inter } from "next/font/google";
+import Nav from "./components/nav";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "CURoomie",
+  description: "The Ultimate Destination for Finding the Perfect Roommate at Cornell!",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <>
+      <head>
+        <link rel="icon" href="/img/icon.png" sizes="any" />
+      </head>
+      <html>
+        <body className={inter.className}>
+          <Nav/>
+          {children}
+        </body>
+      </html>
+    </>
+  );
+}
