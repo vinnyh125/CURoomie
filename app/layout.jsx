a28e09da3e787b51660dda24ab20bdda8d53de6f
NotVinnny <vh222@cornell.edu>
2024-04-18 22:52:05 -0400

    Footer
    + set up footer component and set up in layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import Nav from "./components/nav";
+import Footer from "./components/footer";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -19,6 +20,7 @@ export default function RootLayout({ children }) {
         <body className={inter.className}>
           <Nav/>
           {children}
+          <Footer/>
         </body>
       </html>
     </>
