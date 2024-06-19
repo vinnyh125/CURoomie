68fcc75fbd59b835bc2ec868e854b4d0c5d0d1c4
Vinny <vh222@cornell.edu>
2024-06-19 12:21:11 -0400

    Apostrophe Fix
    + fixed Vercel deployment bug with apostrophe

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,7 @@ export default function Home() {
     <>
       <div className="flex min-w-screen min-h-screen items-center justify-center">
         <div>
-          Hello there, roommate. You're stuck with me for the rest of the year.
+          Hello there, roommate. You&apos;re stuck with me for the rest of the year.
         </div>
       </div>
     </>
