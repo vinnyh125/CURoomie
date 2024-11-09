1ec8a83898a381bb577f42241a8d6e85f793bf1a
Vinny <vh222@cornell.edu>
2024-11-09 17:32:41 -0500

    Custom font changes
    + added staatliches font for titles

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,13 +1,19 @@
-import { Inter } from "next/font/google";
+import { Inter, Staatliches } from "next/font/google";
 import Nav from "./components/nav";
 import Footer from "./components/footer";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
+const staatliches = Staatliches({
+  subsets: ["latin"],
+  weight: "400",
+  variable: "--font-staatliches",
+});
 
 export const metadata = {
   title: "CURoomie",
-  description: "The Ultimate Destination for Finding the Perfect Roommate at Cornell!",
+  description:
+    "The Ultimate Destination for Finding the Perfect Roommate at Cornell!",
 };
 
 export default function RootLayout({ children }) {
@@ -16,11 +22,11 @@ export default function RootLayout({ children }) {
       <head>
         <link rel="icon" href="/img/icon.png" sizes="any" />
       </head>
-      <html>
-        <body className={inter.className}>
-          <Nav/>
+      <html className={inter.className}>
+        <body className={staatliches.variable}>
+          <Nav />
           {children}
-          <Footer/>
+          <Footer />
         </body>
       </html>
     </>
